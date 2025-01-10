bb161de3a904bd5bac7af89a28adecd9c75f80a6
Milena <milenakoros03@gmail.com>
2025-01-10 22:05:53 +0100

    Add logout functionality with styled logout button

--- frontend/src/views/user/userDashboard/UserNav.js
+++ frontend/src/views/user/userDashboard/UserNav.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "../../../styles/User.css"
 
 const UserNav = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        alert("Zostałeś wylogowany!");
+        navigate("/login");
+    };
+
     return (
         <nav>
             <Link to="/user/profile">Profil</Link>
             <Link to="/user/expenses">Wydatki</Link>
             <Link to="/user/expenses/add">Dodaj Wydatek</Link>
             <Link to="/user/categories">Kategorie</Link>
+            <button className="btn-logout" onClick={handleLogout}>
+                Wyloguj się
+            </button>
         </nav>
     );
 };
