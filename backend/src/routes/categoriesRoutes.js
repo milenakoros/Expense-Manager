5a3cc8a80e1f28bb96e7c606470888f4055cc4dd
Milena <milenakoros03@gmail.com>
2025-01-12 18:52:56 +0100

    Delete useless files

--- backend/src/routes/categoriesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCategories, addCategory } = require('../controllers/categoriesController');
-const router = express.Router();
-
-router.get('/', getCategories);
-router.post('/', addCategory);
-
-module.exports = router;
