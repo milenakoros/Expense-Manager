144302788c2da4c60d000e26ba0501c6d2b1c457
Milena <milenakoros03@gmail.com>
2025-01-12 18:50:38 +0100

    Delete useless files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ExpenseList from "./views/expenses/ExpenseList";
-import ExpenseDetails from "./views/expenses/ExpenseDetails";
-import EditExpense from "./views/expenses/EditExpense";
-import AddExpense from "./views/expenses/AddExpense";
 import Register from './views/auth/Register';
 import UserDashboard from "./views/user/userDashboard/UserDashboard";
 import Login from './views/auth/Login';
@@ -23,11 +19,6 @@ const App = () => {
         <Route path="/user/*" element={<UserDashboard />} />
         <Route path="/admin/*" element={<AdminDashboard />} />
         <Route path="/add-article" element={<AddArticle />} />
-
-        <Route path="/expenses/:id" element={<ExpenseDetails />} />
-        <Route path="/add" element={<AddExpense />} />
-        <Route path="/expenses/:id/edit" element={<EditExpense />} />
-        <Route path="/expenses" element={<ExpenseList />} />
       </Routes>
     </Router>
   );
